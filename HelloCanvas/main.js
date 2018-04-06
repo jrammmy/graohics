2e86397138e9fb71131e4dcccc1946c557568299
Justin <justinramiscal@gmail.com>
2018-04-06 13:14:51 -0700

    Clicked points work and right click prints the points nicely to console

--- HelloCanvas/main.js
+++ HelloCanvas/main.js
@@ -63,6 +63,64 @@ function main() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = function(ev) {
+    click(ev, gl, canvas, a_Position);
+    // console.log(ev.button);
+    // Checlk which mouse button is being used
+    if (ev.button === 0) {
+      console.log('Left click detected');
+    } else if (ev.button === 2) {
+      console.log('Right click detected');
+      let count = 1;
+      for (var i = 0; i < g_points.length; i += 2) {
+        console.log(count + ' ' + '(' + g_points[i], g_points[i + 1] + ')');
+        count++;
+      }
+      console.log(g_points);
+    } else {
+      console.log('Middle click detected');
+    }
+  };
+
+  // Disable context menu within the Canvas
+  canvas.addEventListener('contextmenu', function(e) { // Not compatible with IE < 9
+    e.preventDefault();
+  });
+
+}
+
+//-----------------------------------------------------------------------------
+
+var g_points = []; // The array for a mouse press
+
+function click(ev, gl, canvas, a_Position) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
+  y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+  // Store the coordinates to g_points array
+  g_points.push(x);
+  g_points.push(y);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length;
+  for (var i = 0; i < len; i += 2) {
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+
+    // Draw a point
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
+
+  //TODO empty the array to clear the points?
+}
+
+function clearCanvas() {
+  g_points = [];
+  console.log(g_points);
 }
